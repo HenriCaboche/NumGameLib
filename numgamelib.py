60f61223e4a07764a21e3d71b75d167ba26b229c
HenriCaboche <henri.caboche.develop@gmail.com>
2025-05-21 15:48:03 +0200

    Fixed main code libraries and fps placeholder

--- numgamelib.py
+++ numgamelib.py
@@ -1,18 +1,23 @@
-## creates moving objects
+import kandinsky
+import time
+
+# Placeholder :
+fps = 60
+# Creates moving objects
 def create_active(pos_x_o=int, pos_y_o=int,height=int,width=int,velocity_x=int,velocity_y=int,colour=any,backround_col=any,upper_limit_x=int):
     velocity_x= velocity_x
     velocity_y =velocity_y
     pos_x=pos_x_o
     pos_y=pos_y_o
     while True:
-      #path handler
+      # Path handler
         if pos_x > upper_limit_x or pos_x < pos_x_o:
             print("a")
             velocity_x= -1*velocity_x
             velocity_y= -1*velocity_y
             pos_x+=velocity_x
             pos_y+=velocity_y
-          #movement handler
+          # Movement handler
         else:
             kandinsky.fill_rect(pos_x,pos_y,width,height,colour)
             time.sleep(1/fps)
